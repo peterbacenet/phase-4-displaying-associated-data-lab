a358de34246ed4e9a6b9edd78bca7909fd8e0a8d
Peter Bacenet <peterbacenet@icloud.com>
2021-10-14 12:07:07 -0400

    Completed Associated Review Work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+    def show
+        selected_user = User.all.find_by(id: params[:id])
+        render json: selected_user, include: :items
+    end
 end
